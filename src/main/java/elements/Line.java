b1050d4784ff95c74782b6e5b5e3005c3483f0df
Ivano Alvino <ivano.alvino@gmail.com>
2017-05-21 23:58:11 +0200

    Create Line element

---
+++ src/main/java/elements/Line.java
@@ -0,0 +1,30 @@
+package elements;
+
+import canvas.Canvas;
+
+public class Line implements Drawable {
+
+  private int x1;
+
+  private int y1;
+
+  private int x2;
+
+  private int y2;
+
+  public Line(int x1, int y1, int x2, int y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  public void draw(Canvas canvas) {
+    canvas.setFullPoint('X');
+    for (int x = this.x1; x <= this.x2; x++) {
+      for (int y = this.y1; y <= this.y2; y++) {
+        canvas.drawPoint(x, y);
+      }
+    }
+  }
+}
