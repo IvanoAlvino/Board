76da5c70388b5245417bc760be90c07c8ec8f00d
Ivano Alvino <ivano.alvino@gmail.com>
2017-05-24 11:09:43 +0200

    Add negative line test in LineTest

--- src/test/java/elements/LineTest.java
+++ src/test/java/elements/LineTest.java
@@ -14,6 +14,7 @@ public class LineTest {
     Line line = new Line(1, 1, 1, 5);
     line.setFullPoint('x');
     line.draw(canvas);
+
     char linePoint = 'x';
     Assert.assertEquals("Point 1,1 should be filled by line", canvas.getPointAt(0,0), linePoint);
     Assert.assertEquals("Point 2,1 should be filled by line", canvas.getPointAt(1,0), linePoint);
@@ -21,5 +22,20 @@ public class LineTest {
     Assert.assertEquals("Point 4,1 should be filled by line", canvas.getPointAt(3,0), linePoint);
     Assert.assertEquals("Point 5,1 should be filled by line", canvas.getPointAt(4,0), linePoint);
     Assert.assertNotEquals("Point 6,1 should be empty", canvas.getPointAt(0,6), linePoint);
+  }
+
+  @Test
+  public void negativePointsLineIsNotDrawed() throws IllegalWidthORHeightException {
+    Canvas canvas = new Canvas(10, 10);
+    Line line = new Line(-1, -7, -1, -20);
+    line.setFullPoint('x');
+    canvas.setEmptyPoint(' ');
+    line.draw(canvas);
+
+    for (int r = 0; r < canvas.getHeight(); r++) {
+      for (int c = 0; c < canvas.getWidth(); c++) {
+        Assert.assertEquals("Point " + r + "," + c + " must be empty", ' ', canvas.getPointAt(r,c));
+      }
+    }
   }
 }
